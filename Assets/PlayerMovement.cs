2eecca6a8a8f79c2894c082c1679a1e89b02b289
Braxton Lamey <braxlamey@gmail.com>
2021-11-17 09:47:21 -0500

    Andrew's Additions

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    public float movementSpeed = 5f;
+    public void HandleMovement() {
+    float moveX = 0f;
+    float moveY = 0f;
+    if (Input.GetKey(KeyCode.W))
+    {
+      moveY = +1f;
+    }
+    if(Input.GetKey(KeyCode.S))
+    {
+      moveY = -1f;
+    }
+    if (Input.GetKey(KeyCode.A))
+    {
+      moveX = -1f;
+    }
+    if (Input.GetKey(KeyCode.D))
+    {
+      moveX = +1f;
+    }
+    bool isIdle = moveX == 0 && moveY == 0;
+        if (isIdle)
+        {
+            
+        }
+        else
+        {
+            Vector3 moveDir = new Vector3(moveX, moveY).normalized;
+            Vector3 targetMovePosition = transform.position + moveDir * movementSpeed * Time.deltaTime;
+            RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, moveDir, movementSpeed * Time.deltaTime);
+            if (raycastHit.collider == null)
+            {
+                transform.position = targetMovePosition;
+            }
+            else
+            {
+                //Don't move
+                Vector3 testMoveDir = new Vector3(moveDir.x, 0f).normalized;
+            }
+        }
+    }
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude); // Can also use regular magnitude here, but less efficient
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/SwordReflect.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordReflect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private Vector2 inDirection;
+    //private float speed = 1f;
+    public Animator animator;
+
+    public Transform attackCollider;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    void Start()
+    {
+        //inDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        //Attack animation
+        animator.SetTrigger("Attack");
+        //Detect enemies/bullet
+        //START BACK HERE
+        Physics2D.OverlapCircleAll(attackCollider.position, attackRange, enemyLayers);
+    }
+    /*public void fixedUpdate()
+    {
+        transform.Translate(inDirection * speed, Space.World);
+    }
+    public void onCollisionEnter2D(Collision2D collision)
+    {
+        var contact = collision.GetContact(0).point;
+        //var contact = collision.contacts[0].point;
+        Vector2 locateBullet = transform.position;
+        var inNormal = (locateBullet - contact).normalized;
+        inDirection = Vector2.Reflect(inDirection, inNormal);
+
+    }
+    // Make sword slashing animation, somehow make quarter-end animation in order to time reflection
+    //rather than just pressing a key, because that causes the character to be invulnerable
+}
+*/
+}
